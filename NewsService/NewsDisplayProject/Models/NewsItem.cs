96ffde74381f0d8db779fba933bccbff47e46b1b
RinaSchreiber <133571822+RinaSchreiber@users.noreply.github.com>
2024-04-10 17:10:49 +0300

    Delete NewsService directory

--- NewsService/NewsDisplayProject/Models/NewsItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NewsDisplay.Models
-{
-    public class NewsItem
-    {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Link { get; set; }
-    }
-}
